87dc30978741209696cfce90fd966e945fa37908
Bianca Besciu <bianca.besciu@gmail.com>
2018-06-13 17:31:06 +0200

    added validation select function

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
-function validateForm() {
-    let form = document.getElementById('myForm');
+// Validate Selected Countries
+function validateSelect(x){
 
+    // When a new <option> is selected
+    if(document.getElementById(x).selectedIndex !== 0){
+
+        // if true change the box background to green
+        document.getElementById(x).style.background ='#ccffcc';
+
+        //hide error prompt
+        document.getElementById(x + '-error').style.display = "none";
+        return true;
+
+    }else{
+
+        // if false change the box background to red
+        document.getElementById(x).style.background ='#e35152';
+        return false;
+    }
 }
 
+function validateForm () {
+    //Set error catcher
+    let error = 0;
+    let answer = 0;
 
 
-$(document).ready(function() {
-    $('.datepicker').datepicker({
-        format: 'mm/dd/yyyy',
-        startDate: '-3d'
-    });
-});
 
+    //Validate select box
+    if(!validateSelect('country')) {
+        document.getElementById('country-error').style.display = "block";
+        error++;
+    }
+
+    // Don't submit form if there are errors
+    if(error > 0){
+        return false;
+    }
+}
+
+//The handler function to be called when the window's load event fires
+window.onload = function() {
+
+    document.getElementById('myForm')[0].addEventListener('submit', function() {
+        return validateForm();
+    })
+};
